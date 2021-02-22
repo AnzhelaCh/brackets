f6524cf0334fe1e0fab32cbe5589767d81dfbba9
Chernenko Anzhela <chelsea@templatemonster.me>
2021-02-22 18:12:40 +0200

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let count = 0;
+    let item = 1;
+    while(item == 1){
+        item = 0;
+        for( let j=0; j < bracketsConfig.length;j++){
+            let arr = str.split(bracketsConfig[j][0]+bracketsConfig[j][1]);
+            if(arr.length > 1){
+                count++
+                item = 1;
+                str = arr.join('');
+            }
+        }
+    }
+    let result = false;
+    if(str == ''){
+        result = true;
+    }
+    return result;
 }
